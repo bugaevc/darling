c9d12bab4c86000c645edf193b1c45c315557c19
Sergey Bugaev <bugaevc@gmail.com>
2017-01-01 18:33:41 +0300

    Revert "Debug: dump argc at the start of dyld:main"
    This reverts commit 174e4db65857d7094932983ad069752d87552d8d.

--- src/dyld/dyld.cpp
+++ src/dyld/dyld.cpp
@@ -47,13 +47,12 @@ using namespace Darling;
 
 int main(int argc, char** argv, char** envp)
 {
-	printf("argc = %d\n", argc);
 	if (argc < 2 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
 	{
 		printHelp(argv[0]);
 		return 1;
 	}
-
+	
 	if (!HasUserDirectoryStructure())
 		SetupUserDirectoryStructure();
 
@@ -62,7 +61,7 @@ int main(int argc, char** argv, char** envp)
 		MachOMgr* mgr = MachOMgr::instance();
 		std::string executable, unprefixed_argv0;
 		const char* pretendArgv0;
-
+		
 		pretendArgv0 = findFakeArgv0(argv[1]);
 
 		executable = locateBundleExecutable(argv[1]);
@@ -95,7 +94,7 @@ int main(int argc, char** argv, char** envp)
 			__prefix_set(path);
 			unprefixed_argv0 = __prefix_untranslate_path(argv[1], strlen(argv[1]));
 		}
-
+		
 		if (getenv("DYLD_GDBJIT") != nullptr)
 			Darling::SetupGDBJIT();
 
@@ -123,7 +122,7 @@ int main(int argc, char** argv, char** envp)
 		else
 		{
 			MachOObject* obj;
-
+			
 			if (::mmap(0, 4096, PROT_READ|PROT_EXEC, MAP_FIXED|MAP_PRIVATE|MAP_ANONYMOUS, -1, 0) != 0)
 			{
 				if (errno == EPERM)
@@ -139,12 +138,12 @@ int main(int argc, char** argv, char** envp)
 				throw std::runtime_error("This is not a Mach-O executable; dynamic libraries, "
 				"kernel extensions and other Mach-O files cannot be executed with dyld");
 			}
-
+		
 			if (pretendArgv0 != nullptr)
 				argv[1] = (char*) pretendArgv0;
 			else if (!unprefixed_argv0.empty())
 				argv[1] = (char*) unprefixed_argv0.c_str();
-
+			
 			obj->setCommandLine(argc-1, &argv[1], envp);
 
 			obj->load();
@@ -162,12 +161,12 @@ static std::string pathToSelf()
 {
 	char buf[4096];
 	int rd;
-
+	
 	rd = readlink("/proc/self/exe", buf, sizeof(buf)-1);
 	if (rd <= 0)
 		return std::string();
 	buf[rd] = 0;
-
+	
 	return std::string(buf);
 }
 
@@ -202,9 +201,9 @@ static std::string locateBundleExecutable(std::string bundlePath)
 	{
 		std::regex re(".*/([^\\.]+)\\.app/?$", std::regex::icase);
 		std::smatch match;
-
+		
 		std::string myBundlePath = "./" + bundlePath; // TODO: fix the regexp to work without this
-
+		
 		if (std::regex_match(myBundlePath, match, re))
 		{
 			std::stringstream ss;
@@ -246,7 +245,7 @@ static bool isELF(const char* path)
 static const char* findFakeArgv0(char* a0)
 {
 	char* excl;
-
+	
 	excl = strchr(a0, '!');
 	if (excl == nullptr)
 		return nullptr;
